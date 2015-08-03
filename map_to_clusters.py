07aca7d6cbe1c78269a63c70964e1f320b7543bc
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-08-03 16:52:43 +0000

    Extract training and test data from file

--- map_to_clusters.py
+++ map_to_clusters.py
@@ -54,15 +54,43 @@ def sort_columns(data, original, desired):
   new_column_ordering = lookup_column_indices(desired)
   return data[:,new_column_ordering]
 
-def get_training_data(input_file):
-  sample_names, row_labels, data = get_data(input_file)
-  training_sample_names = sample_names[:N_SAMPLES]
-  training_data = sort_columns(data, sample_names, training_sample_names)
+def filter_rows(data, original, desired):
+  """Filters data keeping rows from "original" which are in "desired"
 
-  feature_names = numpy.sort(training_sample_names)
-  training_data = sort_rows(training_data, row_labels, feature_names) 
+  Preserves order of the retained rows in the "original" ordering"""
+  check_in_desired = numpy.vectorize(lambda el: el in desired)
+  rows_to_keep = check_in_desired(original)
+  return data[rows_to_keep,:]
+
+def filter_row_labels(row_labels, desired):
+  check_in_desired = numpy.vectorize(lambda el: el in desired)
+  labels_to_keep = check_in_desired(row_labels)
+  return row_labels[labels_to_keep]
+
+def get_training_data(input_file):
+  """Returns data with a row for each training sample and a column for each
+  feature.
+
+  Columns are sorted alphabetically by feature name; rows remain unsorted.  The
+  names of the training samples are taken to be the first N_SEQUENCES samples
+  listed in the header row of input_file; the rest are assumed to be test
+  data"""
+  sample_names, row_labels, data = get_data(input_file)
+  feature_names = numpy.sort(sample_names[:N_SAMPLES])
+  training_data = sort_columns(data, sample_names, feature_names)
+  training_data = filter_rows(training_data, row_labels, feature_names)
+  training_sample_names = filter_row_labels(row_labels, feature_names)
 
   return training_sample_names, feature_names, training_data
+
+def get_testing_data(input_file, feature_names):
+  sample_names, row_labels, data = get_data(input_file)
+  testing_names = numpy.sort(sample_names[N_SAMPLES:])
+  testing_data = sort_columns(data, sample_names, feature_names)
+  testing_data = filter_rows(testing_data, row_labels, testing_names)
+  testing_sample_names = filter_row_labels(row_labels, testing_names)
+
+  return testing_sample_names, testing_data
 
 if __name__ == '__main__':
   sample_sets = get_sample_names()
@@ -75,6 +103,14 @@ class TestAll(unittest.TestCase):
     original = numpy.array(['third','second','first','forth','fifth']).astype('str')
     desired = numpy.array(['first','second','third']).astype('str')
     actual = sort_rows(data, original, desired)
+    numpy.testing.assert_array_almost_equal(actual, expected)
+
+  def test_filter_rows(self):
+    expected = numpy.array([[3,2,1]]).astype('float').transpose()
+    data = numpy.array([[3,2,1,4,5]]).astype('float').transpose()
+    original = numpy.array(['third','second','first','forth','fifth']).astype('str')
+    desired = numpy.array(['first','second','third']).astype('str')
+    actual = filter_rows(data, original, desired)
     numpy.testing.assert_array_almost_equal(actual, expected)
 
   def test_sort_columns(self):
@@ -109,11 +145,31 @@ test_3	0.3	0.5	0.0	0.9
 train_2	0.2	0.0	0.5	0.8
 train_1	0.0	0.2	0.3	0.7
 test_4	0.7	0.8	0.9	0.0""")
-    expected_sample_names = numpy.array(['train_1', 'train_2']).astype('str')
+    expected_sample_names = numpy.array(['train_2', 'train_1']).astype('str')
     expected_feature_names = numpy.array(['train_1', 'train_2']).astype('str')
-    expected_training_data = numpy.array([[0.0, 0.2], [0.2, 0.0]]).astype('float')
+    expected_training_data = numpy.array([[0.2, 0.0], [0.0, 0.2]]).astype('float')
     training_sample_names, feature_names, training_data = get_training_data(input_file)
     N_SAMPLES = prev_N_SAMPLES
     numpy.testing.assert_array_equal(training_sample_names, expected_sample_names)
     numpy.testing.assert_array_equal(feature_names, expected_feature_names)
     numpy.testing.assert_array_almost_equal(training_data, expected_training_data)
+
+  def test_get_testing_data(self):
+    global N_SAMPLES
+    prev_N_SAMPLES = N_SAMPLES
+    N_SAMPLES = 2
+    input_file = StringIO("""\
+id	train_1	train_2	test_3	test_4
+test_3	0.3	0.5	0.0	0.9
+train_2	0.2	0.0	0.5	0.8
+train_1	0.0	0.2	0.3	0.7
+test_4	0.7	0.8	0.9	0.0""")
+    feature_names = numpy.array(['train_1', 'train_2']).astype('str')
+
+    expected_sample_names = numpy.array(['test_3', 'test_4']).astype('str')
+    expected_testing_data = numpy.array([[0.3, 0.5], [0.7, 0.8]]).astype('float')
+    testing_sample_names, testing_data = get_testing_data(input_file,
+                                                          feature_names)
+    N_SAMPLES = prev_N_SAMPLES
+    numpy.testing.assert_array_equal(testing_sample_names, expected_sample_names)
+    numpy.testing.assert_array_almost_equal(testing_data, expected_testing_data)

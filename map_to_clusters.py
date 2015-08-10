af6aae78f0c709e7ff3d7669926d0f5fee4b1449
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-08-10 13:03:13 +0100

    Use argparse in map_to_cluster; explicitly specify training and test subsets

--- map_to_clusters.py
+++ map_to_clusters.py
@@ -1,42 +1,22 @@
+#!/usr/bin/env python
+
+import argparse
 import csv
 import logging
 import numpy
+import sys
 import unittest
 
 from sklearn import metrics
 from sklearn.cluster import KMeans
 from StringIO import StringIO
 
+from split_other_samples import Split
+from sample_stats import parse_sample
+
 logging.basicConfig(level=logging.DEBUG)
 
-NUMBER_OF_CLUSTERS = 27
-N_SAMPLES = 1000
-
-cluster_matrices = ["CIDRa.Matrix.1000ac.txt", "CIDRa.Matrix.1000bc.txt"]
-
-def get_sample_names():
-  sample_sets = {}
-  for filename in cluster_matrices:
-    sample_file  = open(filename, 'r')
-    header_row = csv.reader(sample_file, delimiter='\t').next()
-    sample_names = header_row[1:]
-    sample_sets[filename] = set(sample_names)
-    sample_file.close()
-  return sample_sets
-
-def print_sample_similarity_matrix(sample_sets):
-  print "\t".join([""] + sample_sets.keys())
-  for i,(filename_a, set_a) in enumerate(sample_sets.items()):
-    row = [filename_a] + [""]*i
-    for filename_b,set_b in sample_sets.items()[i:]:
-      row.append(len(set_a.intersection(set_b)))
-    print "\t".join(map(str, row))
-
-def get_training_sample_names(sample_names):
-  return sample_names[:N_SAMPLES]
-
-def get_test_sample_names(sample_names):
-  return sample_names[N_SAMPLES:]
+DEFAULT_NUMBER_OF_CLUSTERS=10
 
 def get_data(input_file):
   input_file.seek(0)
@@ -86,7 +66,19 @@ def filter_column_labels(column_labels, desired):
   labels_to_keep = check_in_desired(column_labels)
   return column_labels[labels_to_keep]
 
-def get_training_data(input_file):
+def get_relevant_sample_names(sample_names, desired_isolates):
+  """Returns a list of the samples from the desired isolates"""
+  def isolate_name(sample_name):
+    isolate, gene, domain = parse_sample(sample_name)
+    return isolate
+
+  def is_desired_sample(sample_name):
+    return isolate_name(sample_name) in desired_isolates
+
+  desired_samples = filter(is_desired_sample, sample_names)
+  return sorted(desired_samples)
+
+def get_training_data(input_file, isolate_names):
   """Returns data with a row for each training sample and a column for each
   feature.
 
@@ -96,7 +88,7 @@ def get_training_data(input_file):
   data"""
   logging.info("Getting training data")
   sample_names, row_labels, data = get_data(input_file)
-  feature_names = numpy.sort(sample_names[:N_SAMPLES])
+  feature_names = get_relevant_sample_names(sample_names, isolate_names)
   training_data = sort_columns(data, sample_names, feature_names)
   training_data = filter_rows(training_data, row_labels, feature_names)
   training_sample_names = filter_row_labels(row_labels, feature_names)
@@ -105,10 +97,10 @@ def get_training_data(input_file):
                 (len(training_sample_names), len(feature_names)))
   return training_sample_names, feature_names, training_data
 
-def get_testing_data(input_file, feature_names):
+def get_testing_data(input_file, feature_names, isolate_names):
   logging.info("Getting testing data")
   sample_names, row_labels, data = get_data(input_file)
-  testing_names = numpy.sort(sample_names[N_SAMPLES:])
+  testing_names = get_relevant_sample_names(sample_names, isolate_names)
   testing_data = sort_columns(data, sample_names, feature_names)
   testing_data = filter_rows(testing_data, row_labels, testing_names)
   testing_sample_names = filter_row_labels(row_labels, testing_names)
@@ -117,10 +109,15 @@ def get_testing_data(input_file, feature_names):
                 (len(testing_sample_names), len(feature_names)))
   return testing_sample_names, testing_data
 
-def train_classifier(training_data, k=NUMBER_OF_CLUSTERS):
-  classifier = KMeans(n_clusters=k, n_init=100, max_iter=600, n_jobs=1)
+def _train_classifier(training_data, k=DEFAULT_NUMBER_OF_CLUSTERS, n_init=10, max_iter=300):
+  classifier = KMeans(n_clusters=k, n_init=n_init, max_iter=max_iter, n_jobs=3)
   classifier.fit(training_data)
   logging.debug("Trained a classifier with %s clusters" % k)
+  return classifier
+
+def get_classifier_trainer(n_init=10, max_iter=300):
+  def classifier(data, k):
+    return _train_classifier(data, k=k, n_init=n_init, max_iter=max_iter)
   return classifier
 
 def get_cluster_sizes(sample_labels, k):
@@ -130,19 +127,43 @@ def get_cluster_sizes(sample_labels, k):
   return sorted(cluster_sizes.values())
 
 if __name__ == '__main__':
-  output_file = open("results.100init.600iter.log", 'wa')
+  parser = argparse.ArgumentParser()
+  parser.add_argument('split_file', type=argparse.FileType('r'))
+  parser.add_argument('matrix_file_a', type=argparse.FileType('r'))
+  parser.add_argument('matrix_file_b', type=argparse.FileType('r'))
+  parser.add_argument('test_subset', type=str)
+  parser.add_argument('training_subset_a', type=str)
+  parser.add_argument('training_subset_b', type=str)
+  parser.add_argument('output_file', type=argparse.FileType('wa'), default=sys.stdout)
+  parser.add_argument('-f', '--min-k', type=int, default=10)
+  parser.add_argument('-t', '--max-k', type=int, default=50)
+  parser.add_argument('-i', '--max-iters', type=int, default=300)
+  parser.add_argument('-s', '--cluster-seeds', type=int, default=10)
+  parser.add_argument('-r', '--repetitions', type=int, default=10)
+  
+  args = parser.parse_args()
 
-  sample_sets = get_sample_names()
-  print_sample_similarity_matrix(sample_sets)
-  input_file_a, input_file_b = [open(fname, 'r') for fname in cluster_matrices]
-  training_sample_names_a, feature_names_a, training_data_a = get_training_data(input_file_a)
-  test_sample_names_a, testing_data_a = get_testing_data(input_file_a, feature_names_a)
-  training_sample_names_b, feature_names_b, training_data_b = get_training_data(input_file_b)
-  test_sample_names_b, testing_data_b = get_testing_data(input_file_b, feature_names_b)
-  for k in xrange(25,30):
+  splits = {split.name: split for split in Split.from_file(args.split_file)}
+  
+  test_isolates = splits[args.test_subset].isolates
+  training_isolates_a = splits[args.training_subset_a].isolates
+  training_isolates_b = splits[args.training_subset_b].isolates
+
+  training_sample_names_a, feature_names_a, training_data_a = get_training_data(args.matrix_file_a, training_isolates_a)
+  logging.debug("Shape of training_data_a is %s by %s" % training_data_a.shape)
+  test_sample_names_a, testing_data_a = get_testing_data(args.matrix_file_a, feature_names_a, test_isolates)
+  logging.debug("Shape of testing_data_a is %s by %s" % testing_data_a.shape)
+  training_sample_names_b, feature_names_b, training_data_b = get_training_data(args.matrix_file_b, training_isolates_b)
+  logging.debug("Shape of training_data_b is %s by %s" % training_data_b.shape)
+  test_sample_names_b, testing_data_b = get_testing_data(args.matrix_file_b, feature_names_b, test_isolates)
+  logging.debug("Shape of testing_data_b is %s by %s" % testing_data_b.shape)
+
+  train_classifier = get_classifier_trainer(n_init=args.cluster_seeds, max_iter=args.max_iters)
+
+  for k in xrange(args.min_k, args.max_k):
     classifier_a_original = train_classifier(training_data_a, k=k)
     training_clusters_a_original = classifier_a_original.predict(training_data_a)
-    for i in xrange(10):
+    for i in xrange(args.repetitions):
       classifier_a = train_classifier(training_data_a, k=k)
       training_clusters_a = classifier_a.predict(training_data_a)
       # Check if input_file_a is clustered consistently
@@ -160,8 +181,8 @@ if __name__ == '__main__':
       cluster_sizes = ",".join(map(str,get_cluster_sizes(testing_clusters_a, k)))
 
       output_line = "\t".join(map(str,[k,internal_consistency_score,test_data_consistency_score,i,cluster_sizes]))
-      output_file.write(output_line + "\n")
-      output_file.flush()
+      args.output_file.write(output_line + "\n")
+      args.output_file.flush()
 
 class TestAll(unittest.TestCase):
   def test_sort_rows(self):
@@ -203,40 +224,65 @@ sample_3	0.3	0.5	0.0""")
     numpy.testing.assert_array_almost_equal(actual_data, expected_data)
 
   def test_get_training_data(self):
-    global N_SAMPLES
-    prev_N_SAMPLES = N_SAMPLES
-    N_SAMPLES = 2
     input_file = StringIO("""\
-id	train_1	train_2	test_3	test_4
-test_3	0.3	0.5	0.0	0.9
-train_2	0.2	0.0	0.5	0.8
-train_1	0.0	0.2	0.3	0.7
-test_4	0.7	0.8	0.9	0.0""")
-    expected_sample_names = numpy.array(['train_2', 'train_1']).astype('str')
-    expected_feature_names = numpy.array(['train_1', 'train_2']).astype('str')
+id	train_1.g1.DBLa.1	train_1.g2.DBLa.1	test_3.g3.DBLa.3	test_4.g4.DBLa.4
+test_3.g3.DBLa.3	0.3	0.5	0.0	0.9
+train_1.g2.DBLa.1	0.2	0.0	0.5	0.8
+train_1.g1.DBLa.1	0.0	0.2	0.3	0.7
+test_4.g4.DBLa.4	0.7	0.8	0.9	0.0""")
+    expected_sample_names = numpy.array(['train_1.g2.DBLa.1', 'train_1.g1.DBLa.1']).astype('str')
+    expected_feature_names = numpy.array(['train_1.g1.DBLa.1', 'train_1.g2.DBLa.1']).astype('str')
     expected_training_data = numpy.array([[0.2, 0.0], [0.0, 0.2]]).astype('float')
-    training_sample_names, feature_names, training_data = get_training_data(input_file)
-    N_SAMPLES = prev_N_SAMPLES
+    training_sample_names, feature_names, training_data = get_training_data(input_file, ['train_1'])
+    numpy.testing.assert_array_equal(training_sample_names, expected_sample_names)
+    numpy.testing.assert_array_equal(feature_names, expected_feature_names)
+    numpy.testing.assert_array_almost_equal(training_data, expected_training_data)
+
+  def test_get_training_data_subset(self):
+    input_file = StringIO("""\
+id	train_1.g1.DBLa.1	train_2.g2.DBLa.1	test_3.g3.DBLa.3	test_4.g4.DBLa.4
+test_3.g3.DBLa.3	0.3	0.5	0.0	0.9
+train_2.g2.DBLa.1	0.2	0.0	0.5	0.8
+train_1.g1.DBLa.1	0.0	0.2	0.3	0.7
+test_4.g4.DBLa.4	0.7	0.8	0.9	0.0""")
+    expected_sample_names = numpy.array(['train_1.g1.DBLa.1']).astype('str')
+    expected_feature_names = numpy.array(['train_1.g1.DBLa.1']).astype('str')
+    expected_training_data = numpy.array([[0.0]]).astype('float')
+    training_sample_names, feature_names, training_data = get_training_data(input_file, ['train_1'])
     numpy.testing.assert_array_equal(training_sample_names, expected_sample_names)
     numpy.testing.assert_array_equal(feature_names, expected_feature_names)
     numpy.testing.assert_array_almost_equal(training_data, expected_training_data)
 
   def test_get_testing_data(self):
-    global N_SAMPLES
-    prev_N_SAMPLES = N_SAMPLES
-    N_SAMPLES = 2
     input_file = StringIO("""\
-id	train_1	train_2	test_3	test_4
-test_3	0.3	0.5	0.0	0.9
-train_2	0.2	0.0	0.5	0.8
-train_1	0.0	0.2	0.3	0.7
-test_4	0.7	0.8	0.9	0.0""")
-    feature_names = numpy.array(['train_1', 'train_2']).astype('str')
+id	train_1.g1.DBLa.1	train_2.g2.DBLa.2	test_3.g3.DBLa.3	test_4.g4.DBLa.4
+test_3.g3.DBLa.3	0.3	0.5	0.0	0.9
+train_2.g2.DBLa.2	0.2	0.0	0.5	0.8
+train_1.g1.DBLa.1	0.0	0.2	0.3	0.7
+test_4.g4.DBLa.4	0.7	0.8	0.9	0.0""")
+    feature_names = numpy.array(['train_1.g1.DBLa.1', 'train_2.g2.DBLa.2']).astype('str')
 
-    expected_sample_names = numpy.array(['test_3', 'test_4']).astype('str')
+    expected_sample_names = numpy.array(['test_3.g3.DBLa.3', 'test_4.g4.DBLa.4']).astype('str')
     expected_testing_data = numpy.array([[0.3, 0.5], [0.7, 0.8]]).astype('float')
     testing_sample_names, testing_data = get_testing_data(input_file,
-                                                          feature_names)
-    N_SAMPLES = prev_N_SAMPLES
+                                                          feature_names,
+                                                          ['test_3', 'test_4'])
+    numpy.testing.assert_array_equal(testing_sample_names, expected_sample_names)
+    numpy.testing.assert_array_almost_equal(testing_data, expected_testing_data)
+
+  def test_get_testing_data_subset(self):
+    input_file = StringIO("""\
+id	train_1.g1.DBLa.1	train_2.g2.DBLa.2	test_3.g3.DBLa.3	test_4.g4.DBLa.4
+test_3.g3.DBLa.3	0.3	0.5	0.0	0.9
+train_2.g2.DBLa.2	0.2	0.0	0.5	0.8
+train_1.g1.DBLa.1	0.0	0.2	0.3	0.7
+test_4.g4.DBLa.4	0.7	0.8	0.9	0.0""")
+    feature_names = numpy.array(['train_1.g1.DBLa.1', 'train_2.g2.DBLa.2']).astype('str')
+
+    expected_sample_names = numpy.array(['test_3.g3.DBLa.3']).astype('str')
+    expected_testing_data = numpy.array([[0.3, 0.5]]).astype('float')
+    testing_sample_names, testing_data = get_testing_data(input_file,
+                                                          feature_names,
+                                                          ['test_3'])
     numpy.testing.assert_array_equal(testing_sample_names, expected_sample_names)
     numpy.testing.assert_array_almost_equal(testing_data, expected_testing_data)
--- sample_stats.py
+++ sample_stats.py
@@ -5,15 +5,6 @@ import csv
 import json
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('samples_list', type=argparse.FileType('r'), default=sys.stdin)
-
-args = parser.parse_args()
-
-stats = {'samples': {}}
-skipped_samples = 0
-parsed_samples = 0
-
 def parse_sample(sample):
   try:
     isolate, gene, domain, position = sample.split('.')
@@ -21,22 +12,32 @@ def parse_sample(sample):
     raise ValueError("Could not split %s" % sample)
   return isolate, gene, domain
 
-for sample in args.samples_list:
-  sample = sample.strip()
-  try:
-    isolate, gene, domain = parse_sample(sample)
-  except ValueError:
-    skipped_samples += 1
-    continue # Skip this sample
-  else:
-    parsed_samples += 1
-  stats['samples'].setdefault(domain, {}).setdefault(isolate, {'count': 0, 'samples': []})
-  stats['samples'][domain][isolate]['count'] += 1
-  stats['samples'][domain][isolate]['samples'].append(sample)
-
-stats.update({
-  'skipped_samples': skipped_samples,
-  'parsed_samples': parsed_samples
-})
-
-print json.dumps(stats, sort_keys=True, indent=2, separators=(',', ': '))
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('samples_list', type=argparse.FileType('r'), default=sys.stdin)
+  
+  args = parser.parse_args()
+  
+  stats = {'samples': {}}
+  skipped_samples = 0
+  parsed_samples = 0
+  
+  for sample in args.samples_list:
+    sample = sample.strip()
+    try:
+      isolate, gene, domain = parse_sample(sample)
+    except ValueError:
+      skipped_samples += 1
+      continue # Skip this sample
+    else:
+      parsed_samples += 1
+    stats['samples'].setdefault(domain, {}).setdefault(isolate, {'count': 0, 'samples': []})
+    stats['samples'][domain][isolate]['count'] += 1
+    stats['samples'][domain][isolate]['samples'].append(sample)
+  
+  stats.update({
+    'skipped_samples': skipped_samples,
+    'parsed_samples': parsed_samples
+  })
+  
+  print json.dumps(stats, sort_keys=True, indent=2, separators=(',', ': '))

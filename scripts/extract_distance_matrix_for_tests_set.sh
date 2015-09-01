f00797ff09491ded5c88d1493a15f24f3609d6eb
Ben Taylor <ben.taylor@sanger.ac.uk>
2015-09-01 12:54:57 +0100

    Extract subset matrices for all domains

--- scripts/extract_distance_matrix_for_tests_set.sh
+++ scripts/extract_distance_matrix_for_tests_set.sh
@@ -8,7 +8,7 @@ training_set_b=${3-"subset_1"}
 full_matrix_folder=${4-"/nfs/pathogen003/tdo/Pfalciparum/VAR/Assembly.Version2/Normalized.60/Analysis/Domain_Aug_Reblast_may2014"}
 output_folder=${5-$(pwd)}
 
-for domain in CIDRa DBLa; do
+for domain in CIDRa CIDRb CIDRg DBLa DBLb DBLd DBLe DBLg DBLz; do
   full_matrix="${full_matrix_folder}/Matrix.${domain}.txt"
   ./extract_distance_matrix_for_subset.py ../isolates_per_subset/${domain}.subsets $full_matrix $test_set $training_set_a > ${output_folder}/matrix.${domain}.${test_set}_${training_set_a}.txt
   ./extract_distance_matrix_for_subset.py ../isolates_per_subset/${domain}.subsets $full_matrix $test_set $training_set_b > ${output_folder}/matrix.${domain}.${test_set}_${training_set_b}.txt
